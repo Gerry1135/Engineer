1b2e37b76e5b41f6760b3cfad7164d1a32712c0f
Gerry1135 <gerry@madeupplace.com>
2014-06-28 19:27:39 +0100

    Simple object pool

---
+++ Engineer/VesselSimulator/ObjectPool.cs
@@ -0,0 +1,62 @@
+﻿// Kerbal Engineer Redux
+// Author:  Padishar
+// License: Attribution-NonCommercial-ShareAlike 3.0 Unported
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Engineer.VesselSimulator
+{
+    public interface IPooledObject
+    {
+        void FreePooled();
+    }
+
+
+    public class ObjectPool<T> where T : class, IPooledObject, new()
+    {
+        private const int chunkSize = 16;
+        
+        private List<T> freeObjects;
+        private int currentFree;
+        private HashSet<T> usedObjects;
+
+        public ObjectPool()
+        {
+            freeObjects = new List<T>(chunkSize);
+            currentFree = 0;
+            usedObjects = new HashSet<T>();
+        }
+
+        public T Allocate()
+        {
+            T obj;
+            if (currentFree > 0)
+            {
+                obj = freeObjects[--currentFree];
+                freeObjects[currentFree] = null;
+            }
+            else
+                obj = new T();
+
+            usedObjects.Add(obj);
+            return obj;
+        }
+
+        public void Free(T obj)
+        {
+            if (usedObjects.Remove(obj))
+            {
+                obj.FreePooled();
+
+                if (currentFree == freeObjects.Capacity)
+                    freeObjects.Capacity += chunkSize;
+
+                freeObjects[currentFree++] = obj;
+            }
+        }
+    }
+}
